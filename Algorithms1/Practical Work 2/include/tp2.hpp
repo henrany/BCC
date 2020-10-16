4e3fe61f92e6085ff64c17c8fa4beadc58742214
henrany <tmkhenry@gmail.com>
2020-10-16 00:39:32 -0300

    Practical work two done but needs a little refactoring

---
+++ Algorithms1/Practical Work 2/include/tp2.hpp
@@ -0,0 +1,18 @@
+#ifndef TP2_HPP
+#define TP2_HPP
+#include<bits/stdc++.h>
+
+class Diamonds{
+    private:
+    int number;
+    int *diaArray;
+    public:
+    Diamonds(int);
+    void addToVector(int,int);
+    int BruteForce(int n,int,int);
+    int dynamicApproach(int n);
+    void print(int);
+    ~Diamonds();
+};
+
+#endif //TP2_HPP
---
+++ Algorithms1/Practical Work 2/main.cpp
@@ -0,0 +1,30 @@
+#include "tp2.hpp"
+
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        return 0;
+    }
+    int n,value;
+    std::string line;
+    std::ifstream file(argv[1]);
+    if(file.is_open()){
+        file>>n;
+        Diamonds diamonds(n);
+        for(int i=0;i<n;i++){
+            file>>value;
+            diamonds.addToVector(i,value);
+        }
+        auto t1 = std::chrono::high_resolution_clock::now();
+        //diamonds.mergeSort(0,n-1);
+        diamonds.print(n);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        //Execution time of the program
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1 ).count();
+        std::cout<<duration<<"\n";
+        file.close();
+    }
+    else{
+        std::cout<<"No file to be processed now\n";
+    }
+    return 0;
+}
