e823cf9557a06572c52f60da13a8a615321c7481
henrany <tmkhenry@gmail.com>
2020-10-16 00:47:01 -0300

    fixing indentations

--- Algorithms1/Practical Work 2/src/tp2.cpp
+++ Algorithms1/Practical Work 2/src/tp2.cpp
@@ -17,17 +17,15 @@ void Diamonds::addToVector(int index,int value){
 int Diamonds::BruteForce(int n,int S1,int S2){
     // if list becomes empty, return the absolute
 	// difference between two partitions
-	if (n < 0)
+	if (n < 0){
 		return std::abs(S1 - S2);
-
+	}
 	// include current item in the subset S1 and do recursion
 	// for the remaining items (n - 1)
 	int inc = BruteForce(n - 1, S1 + diaArray[n], S2);
-
 	// exclude current item from subset S1 and do recursion
 	// for the remaining items (n - 1)
 	int exc = BruteForce(n - 1, S1, S2 + diaArray[n]);
-
     //return the minimum between them 
 	return std::min(inc, exc);
 }
@@ -38,7 +36,6 @@ int Diamonds::dynamicApproach(int n){
     for(int i=0;i<n;i++){
         sum+=diaArray[i];
     }
-
 	// create a boolean table to store solutions of sub-problems
 	bool lookUp[n + 1][sum + 1];
     for(int i=0;i<n+1;i++){
@@ -46,7 +43,6 @@ int Diamonds::dynamicApproach(int n){
             lookUp[i][j] = false;
         }
     }
-
 	// by using the bottom up approach 
 	for (int i = 0; i <= n; i++){
 		// elements with sum 0 is always true
@@ -54,7 +50,6 @@ int Diamonds::dynamicApproach(int n){
         for (int j = 1; i > 0 && j <= sum; j++){
 			// remove the element from the lookup
 			lookUp[i][j] = lookUp[i - 1][j];
-
 			// include the element in the array 
 			if (diaArray[i - 1] <= j) {
 				//this is done by using bitwise or
@@ -63,7 +58,6 @@ int Diamonds::dynamicApproach(int n){
 			}
 		}
 	}
-
 	// Find maximum value of j between 0 and sum/2 for which last row is true
 	int j = sum / 2;
 	while (j >= 0 && !lookUp[n][j]) {
